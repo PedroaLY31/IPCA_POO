d4dc554abf97bc36da15e6b69e98894691202552
PedroaLY31 <pedro_couto_vc@hotmail.com>
2026-09-24 20:32:51 +0100

    initialized on new bin

--- POO_Aula2/obj/Debug/net8.0/POO_Aula2.AssemblyInfo.cs
+++ POO_Aula2/obj/Debug/net8.0/POO_Aula2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO_Aula2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d862944138c3dc4e623f328e0db3f53ef66dd5e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO_Aula2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO_Aula2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
